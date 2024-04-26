fc6b2b3362b3903201f0b51d4b7bb30b0099f0b4
Rifat N <rifat.nas.07@gmail.com>
2024-04-26 14:08:18 +0200

    create checkboard pattern

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib as plt
+from pattern import Checker, Circle
+
+if __name__ == "__main__":
+    Circle(100, 10,(20, 20))
--- pattern.py
+++ pattern.py
@@ -8,15 +8,32 @@ class Checker:
         self.output = None
 
     def draw(self):
-        tilePerAxis = self.resolution // self.tileSize
-        grid = np.zeros((tilePerAxis, tilePerAxis), dtype=int)
-        grid[::2, ::2] = 1
-        grid[1::2, 1::2] = 1
+        ''' 
+        Let's see an example:
+        5,0     5,1     5,2  !  5,3     5,4     5,5  ! 
+        4,0     4,1     4,2  !  4,3     4,4     4,5  ! 
+        3,0     3,1     3,2  !  3,3     3,4     3,5  ! 
+        ----------------------------------------------
+        2,0     2,1     2,2  !   2,3     2,4     2,5 ! 
+        1,0     1,1     1,2  !   1,3     1,4     1,5 ! 
+        0,0     0,1     0,2  !   0,3     0,4     0,5 ! 
+        
+
+        
+        '''
+        x_coords = np.arange(self.resolution) # resolution of image w.r.t. axis
+        y_coords = np.arange(self.resolution)
+        x_grid, y_grid = np.meshgrid(x_coords, y_coords) # define a grid#
+        x_cond = np.where((x_grid // self.tileSize) % 2 == 0, 1, 0) ## check if int part of index x / tilesize is even
+        y_cond = np.where((y_grid // self.tileSize) % 2 == 0, 1, 0) ## check if int part of index y / tilesize is even
+        condition = (x_cond) ^ (y_cond) # xor condition -> returns 1 matrix of dim(res, res) where cells with value 1
+        #correspond to cells that should be painted
+        grid = np.where(condition, 0, 1)
         self.output = grid
         return np.copy(grid)
 
     def show(self):
-        plt.imshow(self.draw(), cmap='gray')
+        im = plt.imshow(self.draw(), cmap='gray', extent=[0, self.resolution, 0, self.resolution])
         plt.show()
 
 class Circle:
@@ -27,10 +44,10 @@ class Circle:
         self.output = None
     
     def draw(self):
-        x_coords = np.arange(self.resolution)
+        x_coords = np.arange(self.resolution) # resolution of image w.r.t. axis
         y_coords = np.arange(self.resolution)
-        x_grid, y_grid = np.meshgrid(x_coords, y_coords)
-        distance = np.sqrt((x_grid - self.position[0])**2 + (y_grid - self.position[1])**2)
+        x_grid, y_grid = np.meshgrid(x_coords, y_coords) # define a grid#
+        distance = np.sqrt((x_grid - self.position[0])**2 + (y_grid - self.position[1])**2) 
         image = np.where(distance <= self.radius, 255, 0).astype(np.uint8)
         
         return image
@@ -42,7 +59,7 @@ class Circle:
 
 
 if __name__ == "__main__":
-    checker = Checker(10,2)
-    circle = Circle(200,50,(100, 50))
+    checker = Checker(12, 3)
+    #circle = Circle(200,50,(100, 50))
     plot = checker.show()
-    plot2 = circle.show()
+    #plot2 = circle.show()
