3b48d1e4c014d2c1a1d3659bcf5af48f5f4f3473
hesamymt <hesam.yademellat@gmail.com>
2024-05-17 15:51:09 +0200

    TestFullyConnected1 works now!

--- exercise1_material/src_to_implement/Layers/FullyConnected.py
+++ exercise1_material/src_to_implement/Layers/FullyConnected.py
@@ -1,6 +1,5 @@
 import numpy as np
 from .Base import BaseLayer
-from Optimization import Optimizers
 
 class FullyConnected(BaseLayer):
 
@@ -10,7 +9,7 @@ class FullyConnected(BaseLayer):
         self.output_size = output_size
         self.trainable = True
         self.weights = np.random.rand(self.input_size + 1, self.output_size)
-        self._optimizers = None
+        self._optimizer = None
 
 
     def forward(self, input_tensor):
@@ -18,22 +17,23 @@ class FullyConnected(BaseLayer):
         self.fistLayer = np.hstack((input_tensor, x0))
         self.lastLayer = np.dot(self.fistLayer, self.weights)
         return self.lastLayer
-    
-    @property
-    def optimizers(self):
-        return self._optimizers
-    
-    @optimizers.setter
-    def optimizers(self, optimizers):
-        self._optimizers = optimizers
-    
+
 
     def backward(self, error_tensor:np.ndarray):
         self.gradient_bias = np.dot(self.weights, error_tensor.T)
         self.gradient_weights = np.dot(self.fistLayer.T, error_tensor)
         
-        if self._optimizers is not None:
-            self.weights = self._optimizers.calculate_update(self.weights, self.gradient_weights)
+        if self._optimizer is not None:
+            self.weights = self._optimizer.calculate_update(self.weights, self.gradient_weights)
         
         error_output = np.dot(error_tensor, self.weights.T)
         return error_output[:, :-1]
+
+
+    @property
+    def optimizer(self):
+        return self._optimizer
+    
+    @optimizer.setter
+    def optimizer(self, optimizer):
+        self._optimizer = optimizer
